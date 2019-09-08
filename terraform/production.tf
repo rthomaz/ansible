b7d3c027096e0f945bb3e695a9b51682fa657d74
rthomaz <rodrigothomaz@msn.com>
2019-09-08 17:57:59 -0300

    ok

--- terraform/production.tf
+++ terraform/production.tf
@@ -5,14 +5,33 @@ provider "docker" {
 
 # create influxdb container
 resource "docker_container" "influxdb" {
-  name  = "influxdb-2"
+  name  = "influxdb-1"
   image = "influxdb"
   restart = "always"
-  mounts = {
+
+  env = ["INFLUXDB_DB=openhab", "INFLUXDB_ADMIN_USER=openhab", "INFLUXDB_ADMIN_PASSWORD=openhab", "INFLUXDB_USER=openhab", "INFLUXDB_USER_PASSWORD=openhab"]
+
+  mounts {
     target = "/var/lib/influxdb"
     source = "influxdbconfig"
-    type = "bind"
+    type = "volume"
   }
+
+  ports {
+    internal = 8086
+    external = 8086
+  }
+
+  networks_advanced {
+    name = "rthomaz-network"
+  }
+
+  log_driver = "fluentd"
+  log_opts = {
+    fluentd-address = "localhost:24224"
+    tag = "influxdb"
+  }
+
 }
 
 # create wordpress container
