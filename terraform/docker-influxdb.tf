76e38a82a4b670b511130939e889fdba5f1c5c62
rthomaz <rodrigothomaz@msn.com>
2019-09-09 16:32:26 -0300

    ok

--- terraform/docker-influxdb.tf
+++ terraform/docker-influxdb.tf
@@ -47,7 +47,7 @@ resource "docker_container" "influxdb" {
   }
 
   provisioner "local-exec" {
-    command = "sleep 20; ansible-playbook -i /rthomaz/ansible-codes/production /rthomaz/ansible-codes/grafana.yml"
+    command = "sleep 5; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i /rthomaz/ansible-codes/production /rthomaz/ansible-codes/grafana.yml -u rodrigo"
   }
 
 }
