40a21b8f11375ea7f3ae9b9cb321b2b2d6753a99
rthomaz <rodrigothomaz@msn.com>
2019-09-10 11:30:58 -0300

    ok

--- roles/docker-jenkins/templates/executors.groovy
+++ roles/docker-jenkins/templates/executors.groovy
@@ -1,5 +1,2 @@
 import jenkins.model.*
 Jenkins.instance.setNumExecutors(5)
-
-
--Djenkins.install.runSetupWizard=false 
--- terraform/docker-jenkins.tf
+++ terraform/docker-jenkins.tf
@@ -6,7 +6,7 @@ locals {
 
 resource "docker_container" "jenkins" {
   name  = "jenkins"
-  image = "rthomaz/jenkins:0.0.5"
+  image = "rthomaz/jenkins:0.0.6"
   restart = "always"
   count = 1
 
