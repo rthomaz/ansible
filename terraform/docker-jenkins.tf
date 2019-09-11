b77f7ac86c39eb31a4dd29a2467e229d741951fc
rthomaz <rodrigothomaz@msn.com>
2019-09-10 22:42:46 -0300

    ok

--- terraform/docker-jenkins.tf
+++ terraform/docker-jenkins.tf
@@ -6,7 +6,7 @@ locals {
 
 resource "docker_container" "jenkins" {
   name  = "jenkins"
-  image = "rthomaz/jenkins:0.0.16"
+  image = "rthomaz/jenkins:0.0.17"
   restart = "always"
   count = 1
  
@@ -15,11 +15,11 @@ resource "docker_container" "jenkins" {
     "TRY_UPGRADE_IF_NO_MARKER=true"
   ]
   
-   mounts {   
-     target = "/var/jenkins_home"
-     source = "${local.jenkins_volume_source}"
-     type = "bind"
-   }  
+  #  mounts {   
+  #    target = "/var/jenkins_home"
+  #    source = "${local.jenkins_volume_source}"
+  #    type = "bind"
+  #  }  
 
     ports {
         internal = "8080"
