3cbb3c68df66afc052ce1bc76214735ddc466dc5
rthomaz <rodrigothomaz@msn.com>
2019-09-11 02:22:50 -0300

    ok

---
+++ terraform/docker-nexus.tf
@@ -0,0 +1,40 @@
+
+locals {  
+  grafana_port_internal = 8081
+  grafana_port_external = 8081
+  grafana_volume_source = "${var.docker_volumes_folder_path}/nexus"
+  grafana_fluentd_tag = "nexus" 
+}
+
+resource "docker_container" "nexus" {
+  name  = "nexus"
+  image = "sonatype/nexus3"
+  restart = "always"
+  count = 1
+
+  mounts {
+    target = "/nexus-data sonatype/nexus3"
+    source = "${local.nexus_volume_source}"
+    type = "bind"
+  }
+
+  ports {
+    internal = "${local.nexus_port_internal}"
+    external = "${local.nexus_port_external}"
+  }
+
+  networks_advanced {
+    name = "${docker_network.private_network.name}"
+  }
+  
+  # log_driver = "fluentd"
+  # log_opts = {
+  #   fluentd-address = "${var.fluentd_address}"
+  #   tag = "${local.nexus_fluentd_tag}"
+  # }
+
+  # provisioner "local-exec" {
+  #   command = "ansible-playbook -i /rthomaz/ansible-codes/production /rthomaz/ansible-codes/docker-nexus.yml"
+  # }
+
+}
