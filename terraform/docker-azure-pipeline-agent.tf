a7452a43dedb01fc6512adce4543ce76cd20a9ca
rthomaz <rodrigothomaz@msn.com>
2019-09-12 07:28:48 -0300

    ok

--- terraform/docker-azure-pipeline-agent.tf
+++ terraform/docker-azure-pipeline-agent.tf
@@ -16,6 +16,7 @@ resource "docker_container" "azure_pipeline_agent" {
   count = 1
 
   env = [
+    "--sslskipcertvalidation",
     "VSTS_ACCOUNT=${local.azure_pipeline_agent_account}", 
     "VSTS_TOKEN=${local.azure_pipeline_agent_token}", 
     "VSTS_AGENT=${local.azure_pipeline_agent_name}", 
