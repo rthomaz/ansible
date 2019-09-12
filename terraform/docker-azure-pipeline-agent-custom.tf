66adb2135e2e2ed0863a30c789b105472c16afb1
rthomaz <rodrigothomaz@msn.com>
2019-09-12 12:11:04 -0300

    ok

--- terraform/docker-azure-pipeline-agent-custom.tf
+++ terraform/docker-azure-pipeline-agent-custom.tf
@@ -1,12 +1,12 @@
 
 locals {  
-  azure_pipeline_agent_account = "rthomaz"
-  azure_pipeline_agent_token = "7yiyoxrttnmme5tccxm36xbzylleqagj7vef5xkvngh7vfhplo3q"
-  azure_pipeline_agent_name = "docker-azure-pipeline-agent-custom"
-  azure_pipeline_agent_pool = "RThomaz"
-  azure_pipeline_agent_work = "/var/vsts/$VSTS_AGENT"
-  azure_pipeline_agent_volume_source = "${var.docker_volumes_folder_path}/azure_pipeline_agent"  
-  azure_pipeline_agent_fluentd_tag = "azure-pipeline-agent" 
+  azure_pipeline_agent_custom_account = "rthomaz"
+  azure_pipeline_agent_custom_token = "7yiyoxrttnmme5tccxm36xbzylleqagj7vef5xkvngh7vfhplo3q"
+  azure_pipeline_agent_custom_name = "docker-azure-pipeline-agent-custom"
+  azure_pipeline_agent_custom_pool = "RThomaz"
+  azure_pipeline_agent_custom_work = "/var/vsts/$VSTS_AGENT"
+  azure_pipeline_agent_custom_volume_source = "${var.docker_volumes_folder_path}/azure_pipeline_agent"  
+  azure_pipeline_agent_custom_fluentd_tag = "azure-pipeline-agent" 
 }
 
 resource "docker_container" "azure_pipeline_agent_custom" {
@@ -16,16 +16,16 @@ resource "docker_container" "azure_pipeline_agent_custom" {
   count = 1
 
   env = [
-    "VSTS_ACCOUNT=${local.azure_pipeline_agent_account}", 
-    "VSTS_TOKEN=${local.azure_pipeline_agent_token}", 
-    "VSTS_AGENT=${local.azure_pipeline_agent_name}", 
-    "VSTS_POOL=${local.azure_pipeline_agent_pool}", 
-    "VSTS_WORK=${local.azure_pipeline_agent_work}"
+    "VSTS_ACCOUNT=${local.azure_pipeline_agent_custom_account}", 
+    "VSTS_TOKEN=${local.azure_pipeline_agent_custom_token}", 
+    "VSTS_AGENT=${local.azure_pipeline_agent_custom_name}", 
+    "VSTS_POOL=${local.azure_pipeline_agent_custom_pool}", 
+    "VSTS_WORK=${local.azure_pipeline_agent_custom_work}"
   ]
 
 #   mounts {
 #     target = "/var/vsts"
-#     source = "${local.azure_pipeline_agent_volume_source}"
+#     source = "${local.azure_pipeline_agent_custom_volume_source}"
 #     type = "bind"
 #   }
 
@@ -36,11 +36,11 @@ resource "docker_container" "azure_pipeline_agent_custom" {
   # log_driver = "fluentd"
   # log_opts = {
   #   fluentd-address = "${var.fluentd_address}"
-  #   tag = "${local.azure_pipeline_agent_fluentd_tag}"
+  #   tag = "${local.azure_pipeline_agent_custom_fluentd_tag}"
   # }
 
   # provisioner "local-exec" {
-  #   command = "ansible-playbook -i /rthomaz/ansible-codes/production /rthomaz/ansible-codes/docker-azure-pipeline-agent.yml"
+  #   command = "ansible-playbook -i /rthomaz/ansible-codes/production /rthomaz/ansible-codes/docker-azure-pipeline-agent-custom.yml"
   # }
 
 }
