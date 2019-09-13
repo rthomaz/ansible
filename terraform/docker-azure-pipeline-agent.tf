bd1eb3ff5f4923a60aaafd45fe509266292bc448
rthomaz <rodrigothomaz@msn.com>
2019-09-13 18:15:17 -0300

    ok

--- terraform/docker-azure-pipeline-agent.tf
+++ terraform/docker-azure-pipeline-agent.tf
@@ -1,6 +1,6 @@
 
 locals {  
-  azure_pipeline_agent_account = "rthomaz"
+  azure_pipeline_agent_account = "rthomaz-infrastructure"
   azure_pipeline_agent_token = "on7jxjzibpoki24fxqav5ydvzjfljh7dn26kjuax6jix4mjvsmhq"
   azure_pipeline_agent_name = "docker-azure-pipeline-agent"
   azure_pipeline_agent_pool = "RThomaz"
