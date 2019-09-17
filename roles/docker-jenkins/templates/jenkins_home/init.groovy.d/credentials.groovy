8d54abb822fdb2964db3683131d9b835228fd966
rthomaz <rodrigothomaz@msn.com>
2019-09-17 04:06:16 -0300

    ok

--- roles/docker-jenkins/templates/jenkins_home/init.groovy.d/credentials.groovy
+++ roles/docker-jenkins/templates/jenkins_home/init.groovy.d/credentials.groovy
@@ -2,9 +2,28 @@ import com.cloudbees.plugins.credentials.impl.*;
 import com.cloudbees.plugins.credentials.*;
 import com.cloudbees.plugins.credentials.domains.*;
 
-String keyfile = "/tmp/key"
+// get credentials domain
+def domain = Domain.global()
+
+// get credentials store
+def store = SystemCredentialsProvider.getInstance().getStore()
 
 // Azure
-Credentials azure_credential = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,"azureCredentialId", "Credencial do Azure DevOps", "azure", "hcz7vm32sn4y4ufo7kbhaicd7etvsgsiuqr76e5b625mj2nahyqq")
+println "--> Create credentials for Token Azure"
+Credentials azureCredential = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,
+                                    "azureCredentialId", 
+                                    "Credencial do Token do Azure DevOps", 
+                                    "azure", 
+                                    "hcz7vm32sn4y4ufo7kbhaicd7etvsgsiuqr76e5b625mj2nahyqq")
+store.addCredentials(domain, azureCredential)
 
-SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), azure_credential)
+// SSH Docker01
+
+println "--> Create ssh credentials for Docker01"
+creds = new BasicSSHUserPrivateKey(CredentialsScope.GLOBAL,
+                                    "docker01CredentialId",
+                                    "jenkins",
+                                    new BasicSSHUserPrivateKey.FileOnMasterPrivateKeySource("~/.ssh/id_rsa"),
+                                    "definirumasenha",
+                                    "Credencial SSH do Docker01")
+store.addCredentials(domain, creds)
--- terraform/docker-jenkins.tf
+++ terraform/docker-jenkins.tf
@@ -6,7 +6,7 @@ locals {
 
 resource "docker_container" "jenkins" {
   name  = "jenkins"
-  image = "rthomaz/jenkins:0.0.61"
+  image = "rthomaz/jenkins:0.0.62"
   restart = "always"
   count = 1
   
