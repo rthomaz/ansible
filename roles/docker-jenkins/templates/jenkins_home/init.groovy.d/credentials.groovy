d58fb527a24a599856a3228a1b043252e16747fe
rthomaz <rodrigothomaz@msn.com>
2019-09-17 05:02:06 -0300

    ok

--- roles/docker-jenkins/templates/jenkins_home/init.groovy.d/credentials.groovy
+++ roles/docker-jenkins/templates/jenkins_home/init.groovy.d/credentials.groovy
@@ -1,7 +1,9 @@
 import com.cloudbees.plugins.credentials.impl.*;
 import com.cloudbees.plugins.credentials.*;
 import com.cloudbees.plugins.credentials.domains.*;
-
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+import hudson.plugins.sshslaves.*
+  
 // get credentials domain
 def domain = Domain.global()
 
@@ -11,7 +13,7 @@ def store = SystemCredentialsProvider.getInstance().getStore()
 // Azure
 println "--> Create credentials for Token Azure"
 Credentials azureCredential = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,
-                                    "azureCredentialId", 
+                                    "azureCredentialId2", 
                                     "Credencial do Token do Azure DevOps", 
                                     "azure", 
                                     "hcz7vm32sn4y4ufo7kbhaicd7etvsgsiuqr76e5b625mj2nahyqq")
@@ -20,10 +22,10 @@ store.addCredentials(domain, azureCredential)
 // SSH Docker01
 
 println "--> Create ssh credentials for Docker01"
-creds = new BasicSSHUserPrivateKey(CredentialsScope.GLOBAL,
-                                    "docker01CredentialId",
+Credentials docker01Credential = (Credentials) new BasicSSHUserPrivateKey(CredentialsScope.GLOBAL,
+                                    "docker01CredentialId2",
                                     "jenkins",
                                     new BasicSSHUserPrivateKey.FileOnMasterPrivateKeySource("~/.ssh/id_rsa"),
                                     "definirumasenha",
                                     "Credencial SSH do Docker01")
-store.addCredentials(domain, creds)
+store.addCredentials(domain, docker01Credential)
