bb4ef5cf349ed902abdb30965f83c5a6ec2600fb
rthomaz <rodrigothomaz@msn.com>
2019-09-17 15:56:40 -0300

    ok

--- terraform/docker-jenkins.tf
+++ terraform/docker-jenkins.tf
@@ -6,7 +6,7 @@ locals {
 
 resource "docker_container" "jenkins" {
   name  = "jenkins"
-  image = "rthomaz/jenkins:0.0.94"
+  image = "rthomaz/jenkins:0.0.95"
   restart = "always"
   count = 1
   
