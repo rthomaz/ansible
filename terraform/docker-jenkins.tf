1cdf938acfe0f4ba72ded1a4edacbe2a1a6a3f6b
rthomaz <rodrigothomaz@msn.com>
2019-09-17 04:26:10 -0300

    ok

--- terraform/docker-jenkins.tf
+++ terraform/docker-jenkins.tf
@@ -21,14 +21,14 @@ resource "docker_container" "jenkins" {
     type = "bind"
   }  
 
-    ports {
-        internal = "8080"
-        external = "8080"
-    }
-    ports {
-        internal = "50000"
-        external = "50000"
-    }
+  ports {
+      internal = "8080"
+      external = "8080"
+  }
+  ports {
+      internal = "50000"
+      external = "50000"
+  }
      
 
   networks_advanced {
